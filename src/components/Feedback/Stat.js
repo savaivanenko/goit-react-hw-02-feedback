9f7239b7e81b5d7932731930b210dd0627de8d43
Savaivanenko <savaivanenko@gmail>
2021-01-16 23:24:45 +0200

    fixed

--- src/components/Feedback/Stat.js
+++ src/components/Feedback/Stat.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import Notification  from './Notification'
 
-const Stat = ({state, total, positiveFeedback}) => 
-  <div className="Statistic">
-    {/* <h2>Statistics</h2> */}
-    <ul>
-      <li>Good {state.good}</li>  
-      <li>Neutral {state.neutral}</li>  
-      <li>Bad {state.bad}</li>  
-      <li>Total {total}</li>  
-      <li>Positive {positiveFeedback}%</li>  
-    </ul>  
-  </div>
+const Stat = ({good, neutral, bad, total, positiveFeedback}) => {
+  return (
+    <>
+      {total ? 
+        (<ul className="Statistic">
+          <li>Good {good}</li>  
+          <li>Neutral {neutral}</li>  
+          <li>Bad {bad}</li>  
+          <li>Total {total}</li>  
+          <li>Positive {positiveFeedback}%</li>  
+        </ul>)
+       : <Notification/> 
+      }   
+    </>
+  )
+}
 ;
 
 export default Stat;
